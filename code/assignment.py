32e1da89e16494cb6f45b2f0fb7202978613b230
ezra-muratoglu <ezra_muratoglu@brown.edu>
2022-04-23 15:28:24 -0400

    stencil for assignment.py

--- code/assignment.py
+++ code/assignment.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Reshape
 
@@ -12,14 +13,75 @@ class Art_Model(tf.keras.Model):
         self.learning_rate = .01
         self.epochs = None
 
+        input_shape = (self.batch_size, None, None, 3) #to account for different shaped images
+        self.layers = tf.keras.Sequential(
+            tf.keras.layers.conv2D(
+                filters=8, 
+                kernel_size=4,
+                activation='relu',
+                input_shape=input_shape[1:],
+                padding='same'),
+            tf.keras.layers.Dense(self.hidden_dim, activation='relu'),
+            tf.keras.layers.Dropout(.25),
+            tf.keras.layers.Dense(self.hidden_dim, activation='relu'),
+            tf.keras.layers.Dropout(.25),
+            tf.keras.layers.Dense(self.hidden_dim, activation='softmax')
+        )
+
     def call(self, inputs):
-        pass
+        logits = self.layers(inputs)
+        return logits
 
-    def train(model, train_inputs, train_labels):
-        pass
+    def loss(self, logits, labels):
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels, logits)
+        avg_loss = tf.math.reduce_mean(loss)
+        return avg_loss
+    
+    def accuracy(self, logits, labels):
+        correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+        return tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-    def test(model, test_inputs, test_labels):
-        pass
+#-------------------------------------------
+
+def train(model, train_inputs, train_labels):
+        ind = tf.random.shuffle(np.array(range(0, len(train_inputs))))
+        shuffled_inputs = tf.gather(train_inputs, ind)
+        shuffled_labels = tf.gather(train_labels, ind)
+        
+        for i in range(0, int(len(train_inputs)/model.batch_size)):
+            inputs = shuffled_inputs[i*model.batch_size: (i + 1)*model.batch_size]
+            #inputs = tf.image.random_flip_left_right(inputs)
+            
+            labels = shuffled_labels[i*model.batch_size: (i+1)*model.batch_size]
+
+            with tf.GradientTape() as gt:
+                logits = model.call(inputs)
+                loss = model.loss(logits, labels)
+                model.loss_list.append(loss)
+            
+            grad = gt.gradient(loss, model.trainable_variables)
+            model.optimizer.apply_gradients(zip(grad, model.trainable_variables))
+        
+        return
+
+def test(model, test_inputs, test_labels):
+        logits = model.call(test_inputs, is_testing=True)
+        test_accuracy = model.accuracy(logits, test_labels)
+
+        return test_accuracy
+
+def main():
+    train_inputs, train_label, test_inputs, test_labels = get_data('data')
+
+    model=Art_Model()
+
+    for i in range(1, model.epochs):
+        print(f'epoch: {1}')
+        train(model, train_inputs, train_label)
+
+    accuracy = test(model, test_inputs, test_labels)
+    print(f'model accuracy: {accuracy}')
+    pass
 
 
 
